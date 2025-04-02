ceb9548c736acfffc9942593d3a037d9b5c0e3e3
cyhFlight <32890805+cyhFlight@users.noreply.github.com>
2025-04-02 09:49:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,9 @@ from rdkit.Chem import Draw
 
 
 # UI parameters
-ketcher_height = 600
-ketcher_width = 1000
+ketcher_height = 800
+st.set_page_config(layout="wide")
+
 
 
 
@@ -19,7 +20,7 @@ st.title("Ketcher Molecule Editor")
 # Streamlit_ketcher module
 default_mol = "C[N+]1=CC=C(/C2=C3\C=CC(=N3)/C(C3=CC=CC(C(N)=O)=C3)=C3/C=C/C(=C(\C4=CC=[N+](C)C=C4)C4=N/C(=C(/C5=CC=CC(C(N)=O)=C5)C5=CC=C2N5)C=C4)N3)C=C1"
 molecule_str = st.text_input("Molecule", default_mol)
-smiles_code = st_ketcher(molecule_str)
+smiles_code = st_ketcher(molecule_str, height=ketcher_height)
 st.markdown(f"SMILES code: ``{smiles_code}``")
 
 print(smiles_code)
