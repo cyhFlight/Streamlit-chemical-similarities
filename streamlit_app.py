37a1e232c4122cc50e18d6e816c9e6af06178264
cyhFlight <32890805+cyhFlight@users.noreply.github.com>
2025-04-02 09:23:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,25 +4,31 @@ from streamlit_ketcher import st_ketcher
 from rdkit import Chem
 from rdkit.Chem import Draw
 
-# Streamlit 标题
-st.title("Ketcher 分子编辑器")
 
-# Ketcher 的 HTML/JavaScript 嵌入
-## not working https://unpkg.com/ketcher@latest/dist/ketcher.min.js not found
+# UI parameters
+ketcher_height = 600
+ketcher_width = 1000
 
+
+
+st.title("Ketcher Molecule Editor")
+
+## Ketcher HTML/JavaScript embedding not working
+## https://unpkg.com/ketcher@latest/dist/ketcher.min.js not found
+
+# Streamlit_ketcher module
 default_mol = "C[N+]1=CC=C(/C2=C3\C=CC(=N3)/C(C3=CC=CC(C(N)=O)=C3)=C3/C=C/C(=C(\C4=CC=[N+](C)C=C4)C4=N/C(=C(/C5=CC=CC(C(N)=O)=C5)C5=CC=C2N5)C=C4)N3)C=C1"
-molecule = st.text_input("Molecule", default_mol)
-smile_code = st_ketcher(molecule)
-st.markdown(f"Smile code: ``{smile_code}``")
+molecule_str = st.text_input("Molecule", default_mol)
+smiles_code = st_ketcher(molecule_str)
+st.markdown(f"SMILES code: ``{smiles_code}``")
 
-# 从 URL 参数获取 SMILES
-smiles = st.text_input("输入在 Ketcher 中创建的分子 SMILES:", "")
+print(smiles_code)
 
-# 显示分子结构
-if smiles:
+# Use RDkit to draw
+if molecule_str:
     try:
-        mol = Chem.MolFromSmiles(smiles)
+        mol = Chem.MolFromSmiles(molecule_str)
         if mol:
-            st.image(Draw.MolToImage(mol), caption="分子结构", use_column_width=True)
+            st.image(Draw.MolToImage(mol), caption="Molecule structure", use_container_width=True)
     except Exception as e:
-        st.error(f"错误: {e}")
+        st.error(f"Error: {e}")
